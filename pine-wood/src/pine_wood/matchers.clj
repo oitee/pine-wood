905c6beb6ba6410c14906f7830531416dca33743
oitihjya <oitihjya.sen@helpshift.com>
2022-11-04 01:31:25 +0530

    Fix indentation

--- pine-wood/src/pine_wood/matchers.clj
+++ pine-wood/src/pine_wood/matchers.clj
@@ -33,19 +33,19 @@
    [#"\[[\w0-9-]+\]" :index (fn [s]
                               (subs s 1 (dec (count s))))]
    [#"took_millis\[[0-9]+\]" :took_millis (fn [s]
-                                                    (Integer/parseInt (subs s 12 (dec (count s)))))]
+                                            (Integer/parseInt (subs s 12 (dec (count s)))))]
    [#"total_hits\[[0-9+]+ hits\]" :total_hits (fn [s]
-                                                        (subs s 11 (- (count s) 6)))]
+                                                (subs s 11 (- (count s) 6)))]
    [#"search_type\[[\w_]+\]" :search_type (fn [s]
-                                                    (subs s 12 (dec (count s))))]
+                                            (subs s 12 (dec (count s))))]
    [#"total_shards\[[0-9]+\]" :total_shards (fn [s]
-                                                      (Integer/parseInt (subs s 13 (dec (count s)))))]
+                                              (Integer/parseInt (subs s 13 (dec (count s)))))]
    [#"source\[.*\]" :source (fn [s]
-                                      (-> s
-                                          (subs 7 (dec (count s)))
-                                          (clojure.string/replace  "\\\"" "\"")
-                                          (cc/parse-string)
-                                          (cc/generate-string)))]])
+                              (-> s
+                                  (subs 7 (dec (count s)))
+                                  (clojure.string/replace  "\\\"" "\"")
+                                  (cc/parse-string)
+                                  (cc/generate-string)))]])
 (defn extract [line first-try?]
   (let [matchers (if first-try?
                    matchers-1
