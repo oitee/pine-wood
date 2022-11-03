bb48c5b9a86090c3621191ddd9ea3d8755c14a7e
oitihjya <oitihjya.sen@helpshift.com>
2022-11-04 01:27:21 +0530

    Add alternate parser for second set of logs

--- pine-wood/src/pine_wood/matchers.clj
+++ pine-wood/src/pine_wood/matchers.clj
@@ -1,7 +1,7 @@
 (ns pine-wood.matchers
   [:require [cheshire.core :as cc]])
 
-(def matchers
+(def matchers-1
   [[#"\[[\w-]+\]" :index (fn [s] (subs s 1 (dec (count s))))]
    [#"took_millis\[[0-9]+\]" :took_millis (fn [s]
                                             (Integer/parseInt (subs s 12 (dec (count s)))))]
@@ -14,20 +14,47 @@
    ;; Cheeky hack: we must extract id first for source to be findable
    [#"id\[[\w]*\]" :id identity]
    [#"source\[.*\]" :source (fn [s]
-                              (def s* s)
                               (-> s
                                   (subs 7 (dec (count s)))
                                   ;;(clojure.string/replace  "\\\"" "\"")
                                   (cc/parse-string)
-                                  (cc/generate-string)
-                                  ))]])
-
-(defn extract [line]
-  (:result (reduce (fn [{:keys [text result]}
-                [rgx k sanitiser-fn]]
-             (let [matched-str (re-find rgx text)]
-               {:text (clojure.string/replace-first text matched-str "")
-                :result (assoc result k (sanitiser-fn matched-str))}))
-           {:text line
-            :result {}}
-           matchers)))
+                                  (cc/generate-string)))]])
+(def matchers-2
+  [[#"\[[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][A-Z][0-9][0-9]:[0-9][0-9]:[0-9][0-9],[0-9][0-9][0-9]\]"
+    :timestamp
+    (fn [s] (subs s 1 (dec (count s))))]
+   [#"\[[\w]+ ]" :level (fn [s] (subs s 1 (- (count s) 2)))]
+   [#"\[[\w.]+   " :component (fn [s]
+                                (as-> s s*
+                                  (subs s* 1 (dec (count s*)))
+                                  (clojure.string/trim s*)))]
+   [#" \[[\w+0-9-]+\] " :node.name (fn [s]
+                                     (subs s 2 (- (count s) 2)))]
+   [#"\[[\w0-9-]+\]" :index (fn [s]
+                              (subs s 1 (dec (count s))))]
+   [#"took_millis\[[0-9]+\]" :took_millis (fn [s]
+                                                    (Integer/parseInt (subs s 12 (dec (count s)))))]
+   [#"total_hits\[[0-9+]+ hits\]" :total_hits (fn [s]
+                                                        (subs s 11 (- (count s) 6)))]
+   [#"search_type\[[\w_]+\]" :search_type (fn [s]
+                                                    (subs s 12 (dec (count s))))]
+   [#"total_shards\[[0-9]+\]" :total_shards (fn [s]
+                                                      (Integer/parseInt (subs s 13 (dec (count s)))))]
+   [#"source\[.*\]" :source (fn [s]
+                                      (-> s
+                                          (subs 7 (dec (count s)))
+                                          (clojure.string/replace  "\\\"" "\"")
+                                          (cc/parse-string)
+                                          (cc/generate-string)))]])
+(defn extract [line first-try?]
+  (let [matchers (if first-try?
+                   matchers-1
+                   matchers-2)]
+    (:result (reduce (fn [{:keys [text result]}
+                          [rgx k sanitiser-fn]]
+                       (let [matched-str (re-find rgx text)]
+                         {:text (clojure.string/replace-first text matched-str "")
+                          :result (assoc result k (sanitiser-fn matched-str))}))
+                     {:text line
+                      :result {}}
+                     matchers))))
