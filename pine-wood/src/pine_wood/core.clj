c9ecd916805860cdecd6f1cae7bd3e76769d2cf1
oitihjya <oitihjya.sen@helpshift.com>
2022-11-07 18:39:14 +0530

    Refactor and remove print statements

--- pine-wood/src/pine_wood/core.clj
+++ pine-wood/src/pine_wood/core.clj
@@ -12,7 +12,6 @@
   (try (let [{:keys [message] :as full-log} (cc/parse-string line true)
              message (pm/extract message true)
              parsed-log (assoc full-log :message message)]
-         (def parsed-log1* parsed-log)
          (swap! counter assoc :success (inc (:success @counter)))
          (swap! counter assoc :parser-1 (inc (:parser-1 @counter)))
          parsed-log)
@@ -37,26 +36,22 @@
              only-top-keys-map (select-keys parsed-log non-message-keys)
              only-message-keys-map (select-keys parsed-log message-keys)
              parsed-log (merge only-top-keys-map {:message only-message-keys-map})]
-         (def parsed-log2* parsed-log)
          (swap! counter assoc :success (inc (:success @counter)))
          (swap! counter assoc :parser-2 (inc (:parser-2 @counter)))
          parsed-log)
        (catch Exception e
-         (when (clojure.string/starts-with? line "[20" )
-           (def failed-line-from-parse-log-2* line))
+         (when (clojure.string/starts-with? line "[20" ))
          false)))
 
 (defn parse-log [log]
   (or (parse-log-1 log)
       (parse-log-2 log)
-      (do (println "Error.")
-          (swap! counter assoc :failure (inc (:failure @counter)))
-          )))
+      (swap! counter assoc :failure (inc (:failure @counter)))))
 
-(defn do-stuff
-  [source-file out-path]
+(defn prepare-and-add-logs
+  [source-dir out-path]
   (with-open [out-file (clojure.java.io/writer out-path)]
-    (doseq [f (file-seq (clojure.java.io/file source-file))]
+    (doseq [f (file-seq (clojure.java.io/file source-dir))]
       (when (and (.isFile f)
                  (clojure.string/includes? (.getName f) "index_search_slowlog"))
         (let [contents (slurp f)
@@ -70,10 +65,10 @@
 
 
 
-(defn add-logs [source-file out-file]
+(defn add-logs [source-dir out-file]
   (reset! counter {:success 0
                    :failure 0
                    :parser-1 0
                    :parser-2 0})
-  (do-stuff source-file out-file)
+  (prepare-and-add-logs source-dir out-file)
   (println @counter))
